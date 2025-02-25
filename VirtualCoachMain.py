52187319f1d42e2363fecb8ff5b1140050abca0a
JayFerrier <jay.ferrier2022@my.ntu.ac.uk>
2025-02-25 11:22:27 +0000

    Update VirtualCoachMain.py

--- VirtualCoachMain.py
+++ VirtualCoachMain.py
@@ -29,6 +29,8 @@ POSE_PAIRS = [
     [12, 13],
 ]
 
+net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
+
 def loadImages(folderName):
     setOfImages = []
     for files in os.listdir(folderName):
@@ -48,6 +50,7 @@ def processImages(exerciseName):
             im = im[0:400,175:500]
             processedImages.append(im)
         if exerciseName.lower() == "bench_press":
+            im = cv2.resize(im, (480,360))
             im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
             im = im[75:400,75:400]
             processedImages.append(im)
@@ -57,11 +60,90 @@ def processImages(exerciseName):
             processedImages.append(im)
     return processedImages
 
+def poseEstImages(imageList):
+    inim = 1
+    for im in imageList:
+        inWidth  = im.shape[1]
+        inHeight = im.shape[0]
+        netInputSize = (368, 368)
+        inpBlob = cv2.dnn.blobFromImage(im, 1.0 / 255, netInputSize, (0, 0, 0), swapRB=True, crop=False)
+        net.setInput(inpBlob)
+
+        # Forward Pass
+        output = net.forward()
+
+        # Display probability maps
+        #plt.figure(figsize=(20, 5))
+        for i in range(nPoints):
+            probMap = output[0, i, :, :]
+            displayMap = cv2.resize(probMap, (inWidth, inHeight), cv2.INTER_LINEAR)
             
+            # plt.subplot(2, 8, i + 1)
+            # plt.axis("off")
+            # plt.imshow(displayMap, cmap="jet")
+        # X and Y Scale
+        scaleX = inWidth  / output.shape[3]
+        scaleY = inHeight / output.shape[2]
+
+        # Empty list to store the detected keypoints
+        points = []
+
+        # Treshold
+        threshold = 0.1
+
+        for i in range(nPoints):
+            # Obtain probability map
+            probMap = output[0, i, :, :]
+
+            # Find global maxima of the probMap.
+            minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
+
+            # Scale the point to fit on the original image
+            x = scaleX * point[0]
+            y = scaleY * point[1]
+
+            if prob > threshold:
+                # Add the point to the list if the probability is greater than the threshold
+                points.append((int(x), int(y)))
+            else:
+                points.append(None)
+        imPoints = im.copy()
+        imSkeleton = im.copy()
+
+        # Draw points
+        for i, p in enumerate(points):
+            cv2.circle(imPoints, p, 8, (255, 255, 0), thickness=-1, lineType=cv2.FILLED)
+            cv2.putText(imPoints, "{}".format(i), p, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, lineType=cv2.LINE_AA)
+
+        # Draw skeleton
+        for pair in POSE_PAIRS:
+            partA = pair[0]
+            partB = pair[1]
+
+            if points[partA] and points[partB]:
+                cv2.line(imSkeleton, points[partA], points[partB], (255, 255, 0), 2)
+                cv2.circle(imSkeleton, points[partA], 8, (255, 0, 0), thickness=-1, lineType=cv2.FILLED)
+
+        # plt.figure(figsize=(50, 50))
+
+        plt.subplot(4,3,inim)
+        plt.axis("off")
+        plt.imshow(imSkeleton)
+        inim+=1
+    plt.show()
+        
+
+
+
 images = processImages("squat")
 # Display probability maps
+inim=1
 for im in images:
-    plt.figure(figsize=(10, 5))
+    plt.subplot(4,3,inim)
     plt.imshow(im)
+    plt.axis('off')
+    inim += 1
 
-    plt.show()
+
+
+poseEstImages(images)
